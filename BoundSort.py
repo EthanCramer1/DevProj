82e274726960f04ed70a75484fccede01586dce7
Ethan Cramer <cramerethan98@gmail.com>
2020-06-28 10:38:20 -0500

    Update BoundSort.py

--- BoundSort.py
+++ BoundSort.py
@@ -1,26 +1,18 @@
 def insertion_sort(array, left=0, right=None):
-
     if right is None:
-        
         right = len(array) - 1
 
     for i in range(left + 1, right + 1):
-
         key_item = array[i]
         j = i - 1
-
         while j >= left and array[j] > key_item:
-
             array[j + 1] = array[j]
             j -= 1
-
         array [j + 1] = key_item
-
     return array
 
 
 def bound_sort(array, bound):
-
     boundedArray = array[bound[0]:bound[1]+1]
     leftovers = array[bound[1]+1:len(array)]
 
@@ -30,11 +22,8 @@ def bound_sort(array, bound):
     sortedUnboundedArray = insertion_sort(array)
 
     if sortedBoundedArray == sortedUnboundedArray:
-
         return True
-
     else:
-
         return False
     
 
